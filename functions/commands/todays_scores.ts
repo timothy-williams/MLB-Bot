0207777d397de389ec3a65cda22a90aae9088044
Tim Williams <timothy.r.williams@outlook.com>
2023-08-19 17:08:57 -0400

    Rework MLB API logic

--- functions/commands/todays_scores.ts
+++ functions/commands/todays_scores.ts
@@ -4,7 +4,40 @@ import axios from 'axios';
 
 // Display all scores from today - live, scheduled, and completed
 export async function todays_scores() {
-    // Create array for each abstracted game state
+    try {
+        // Create an instance of the Game class
+        const game = new Game();
+
+        // Fetch game Pks for the current date
+        const gamePks = await game.fetchGamePksForCurrentDate();
+
+        if (gamePks.length === 0) {
+            return 'No games scheduled for today.';
+        }
+
+        // Fetch game data for the obtained game Pks
+        const gameDataArray = await game.fetchGameData(gamePks);
+
+        // Process game data and build the final message with game state sorting
+        const gameMessages = await processGameData(gameDataArray);
+
+        // Join all game messages into a single string
+        const finalMessage = gameMessages.join('\n');
+
+        return finalMessage;
+    } catch (error) {
+        console.error('Error fetching today\'s scores:', error);
+        return 'An error occurred while fetching today\'s scores.';
+    }
+}
+
+/**
+ * Process game data and build game messages with game state sorting.
+ * @param gameDataArray An array of game data.
+ * @returns An array of game messages sorted by game state.
+ */
+async function processGameData(gameDataArray: string[]): Promise<string[]> {
+    // Create arrays to separate games by abstract game state
     const live: string[] = [];
     const scheduled: string[] = [];
     const final: string[] = [];
@@ -14,36 +47,30 @@ export async function todays_scores() {
     const today: string = format(new Date(), 'yyyy-MM-dd');
     const todayVerbose: string = `**${format(new Date(), 'MMMM d, yyyy')}**`;
     const endpoint: string = `https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=${today}`;
-    
+
     try {
         const res = await axios.get(endpoint);
         const games = res.data.dates[0].games; // All games from today
 
-        for (const g of games) {
-            const gameState: string = g.status.abstractGameState;
-            const scoreboard = await new Game(g.gamePk).scoreboard();
+        for (let i = 0; i < gameDataArray.length; i++) {
+            const gameData = gameDataArray[i];
+            const gameState: string = games[i].status.abstractGameState;
 
-            // Sort games by game state and retrieve scoreboard() for each
             if (gameState === 'Live') {
-                live.push(scoreboard + '\n');
+                live.push(gameData);
             } else if (gameState === 'Preview') {
-                scheduled.push(scoreboard + '\n');
+                scheduled.push(gameData);
             } else if (gameState === 'Final') {
-                final.push(scoreboard + '\n');
+                final.push(gameData);
             } else {
-                other.push(scoreboard + '\n');
+                other.push(gameData);
             }
         }
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
 
-    // Formatting
-    const diff: number = Math.floor((86 - todayVerbose.length) / 2);
-    const padding: string = '-'.repeat(diff);
-    const todayMsg: string = `${padding}${todayVerbose}${padding}`;
-    const message: string = `${todayMsg}\n${[...live, ...scheduled, ...final, ...other].join('\n')}`;
-    const finalMessage: string = message.slice(0, -1);
-    
-    return finalMessage;
+        // Join and return sorted game messages
+        return [...live, ...scheduled, ...final, ...other];
+    } catch (error) {
+        console.error('Error fetching today\'s games:', error);
+        return [];
+    }
 }
